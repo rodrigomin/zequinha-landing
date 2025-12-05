146199c93c98a0316af87faab7c3fc6b00aa56f7
Rodrigo Minoti Costa <rodrigo.minotic@gmail.com>
2025-12-05 15:48:44 -0300

    fix: [ajustando o docs]

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/zequinha-landing/',
   plugins: [react()],
+  build: {
+    outDir: 'docs', // muda o build do dist para docs
+  },
 })
